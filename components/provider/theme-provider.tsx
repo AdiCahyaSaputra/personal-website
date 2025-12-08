9131a902fb78f5399c60f45aeb02c41d368cada8
Adi <adics631@gmail.com>
2025-12-08 16:29:28 +0700

    fix: build issue

--- components/provider/theme-provider.tsx
+++ components/provider/theme-provider.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import React from "react";
-
-import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { ThemeProviderProps } from "next-themes/dist/types";
+import {
+  ThemeProvider as NextThemesProvider,
+  ThemeProviderProps,
+} from "next-themes";
 
 const ThemeProvider = ({ children, ...props }: ThemeProviderProps) => {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;

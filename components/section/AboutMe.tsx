4b9232c21004f12bc67529e99d5dcc2c2f64279c
Adi <adics631@gmail.com>
2025-12-14 09:40:59 +0700

    fix: update AboutMe component text for clarity and consistency in recent works presentation

--- components/section/AboutMe.tsx
+++ components/section/AboutMe.tsx
@@ -27,36 +27,34 @@ const AboutMe = () => {
         <p>
           I&apos;m from Jakarta, Indonesia. Graduated at 2023 from{" "}
           <b className="text-foreground">Vocational High School</b> majoring in{" "}
-          <b className="text-foreground">Software Enginering</b>. My recent
-          works :{" "}
-          {recentWorks.map((work, idx) => (
-            <Link
-              href={work.link}
-              key={idx}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="underline text-foreground hover:decoration-red-800 mr-1"
-            >
-              {work.name}
-              {idx < recentWorks.length - 1 ? (
-                <span className="text-foreground/75">,</span>
-              ) : (
-                <span className="text-foreground/75">.</span>
-              )}
-            </Link>
-          ))}
+          <b className="text-foreground">Software Enginering</b>.
         </p>
 
         <div className="mt-4 flex items-center gap-1 flex-wrap">
           <p>
-            Explore my other{" "}
+            My recent works :{" "}
+            {recentWorks.map((work, idx) => (
+              <Link
+                href={work.link}
+                key={idx}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="underline text-foreground hover:decoration-red-800 mr-1"
+              >
+                {work.name}
+                {idx < recentWorks.length - 1 && (
+                  <span className="text-foreground/75">,</span>
+                )}
+              </Link>
+            ))}
+            or explore my other{" "}
             <Link
               href="/projects"
               className="underline text-foreground hover:decoration-red-800"
             >
               projects
             </Link>{" "}
-            or see my{" "}
+            and see my{" "}
             <Link
               href="/experience"
               className="underline text-foreground hover:decoration-red-800"

8e26f9264e804803ced394a4b51c827f6fbdbd3d
Adi <adics631@gmail.com>
2026-01-27 17:35:12 +0700

    feat: update cv and some style

--- components/section/Projects.tsx
+++ components/section/Projects.tsx
@@ -3,7 +3,10 @@ import CardProject from "../reusable/projects/CardProject";
 
 const Projects = () => {
   return (
-    <section id="projects" className="grid gap-4 grid-cols-1 lg:grid-cols-2 scroll-mt-26">
+    <section
+      id="projects"
+      className="grid gap-4 grid-cols-1 lg:grid-cols-2 scroll-mt-26"
+    >
       <div>
         <h1 className="text-lg lg:text-2xl font-bold">Other Projects</h1>
         <p className="text-foreground/60">That I'm truly proud to showcase.</p>
